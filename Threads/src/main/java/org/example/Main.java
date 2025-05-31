724c46c862da060ffc009914f865a4a5a3271ffc
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-31 12:14:38 +0530

    Connection Pool

--- Threads/src/main/java/org/example/Main.java
+++ Threads/src/main/java/org/example/Main.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("A");
 
         System.out.println("B");
@@ -15,5 +14,16 @@ public class Main {
         thread.start();
 
         System.out.println("C");
+
+        // Functional Interface
+        Thread thread1 = new Thread(() -> {
+            List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+            numbers.forEach(number -> {
+                System.out.println(number);
+            });
+        });
+
+        thread1.run();
     }
 }
--- Threads/src/main/java/org/example/MyCode.java
+++ Threads/src/main/java/org/example/MyCode.java
@@ -3,10 +3,11 @@ package org.example;
 import java.util.Arrays;
 import java.util.List;
 
+//@FunctionalInterface
 public class MyCode implements Runnable {
     @Override
     public void run() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         numbers.forEach(number -> {
             System.out.println(number);
